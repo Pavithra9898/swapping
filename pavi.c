ac69331cc577e4e5d4381d5543f92d578f5a7bf8
Pavithra9898 <pavithrapavi9395@gmail.com>
2018-10-27 14:42:57 +0530

    updated pavi.c

---
+++ pavi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+	int x=10;
+	int y=5;
+
+	printf("\n values before swapping");
+	printf("\n x: "%d\n y: %d\n",x,y);
+
+	printf("\n values after swapping");
+	printf("\n x: "%d\n y: %d\n",x,y);
+}
