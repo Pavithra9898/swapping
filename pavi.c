8a27ebe4097447da756ee6b392d85843f1150e06
Pavithra9898 <pavithrapavi9395@gmail.com>
2018-10-27 14:50:49 +0530

    Added lolgic

--- pavi.c
+++ pavi.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
+
 void main()
+
 {
-	int x=10;
-	int y=5;
+	
+int x=10;
+	
+int y=5;
 
-	printf("\n values before swapping");
-	printf("\n x: "%d\n y: %d\n",x,y);
+	
+printf("\n values before swapping");
+	
+printf("\n x: "%d\n y: %d\n",x,y);
 
-	printf("\n values after swapping");
-	printf("\n x: "%d\n y: %d\n",x,y);
+
+
+int temp=x;
+x=y;
+y=temp;
+	
+printf("\n values after swapping");
+	
+printf("\n x: "%d\n y: %d\n",x,y);
+
 }
